94016b9355ded2c50df31ecd5cde4b137e947546
Theo van Kraay <theo.van@microsoft.com>
2024-10-24 20:42:43 +0100

    edit

--- startup.sh
+++ startup.sh
@@ -1,4 +1,1 @@
-# startup.sh
-#!/bin/bash
-pip install -r requirements.txt
-streamlit run src/app/cosmos-app.py --server.port=443 --server.address=0.0.0.0
+python -m streamlit run src/app/cosmos-app.py --server.port 8000 --server.address 0.0.0.0
